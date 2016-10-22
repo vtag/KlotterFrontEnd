fc9ad260b5e2696a6643da6a140532fdcd6de986
Ratstache <landonbutterworth@gmail.com>
2016-10-23 03:30:49 +0900

    adds location tracking, adds message posting (front end only)

--- app/src/main/java/vtag/klotter/MainActivity.java
+++ app/src/main/java/vtag/klotter/MainActivity.java
@@ -1,18 +1,49 @@
 package vtag.klotter;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.location.LocationManager;
+import android.location.Location;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.app.AlertDialog;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.text.InputType;
+import android.content.DialogInterface;
+import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
+
+
     private GoogleMap mMap;
+    private Button b;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +53,62 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        b = (Button) findViewById(R.id.button);
+
+        configureBtn();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    void configureBtn(){
+        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
+        b.setOnClickListener(new View.OnClickListener() {
+            private String m_Text;
+
+            @Override
+            public void onClick(final View view) {
+
+                    /* Alert Dialog Code Start*/
+                    AlertDialog.Builder alert = new AlertDialog.Builder(view.getContext());
+                    alert.setTitle("Alert Dialog With EditText"); //Set Alert dialog title here
+                    alert.setMessage("Enter Your Name Here"); //Message here
+
+                    // Set an EditText view to get user input
+                    final EditText input = new EditText(view.getContext());
+                    alert.setView(input);
+
+                    alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            //You will get as string input data in this variable.
+                            // here we convert the input to a string and show in a toast.
+                            String str = input.getEditableText().toString();
+                            m_Text = str;
+                            Toast.makeText(view.getContext(),str,Toast.LENGTH_LONG).show();
+
+                            LocationManager lManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+                            int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(),
+                                    android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+                            Location myLocation = lManager.getLastKnownLocation(lManager.getBestProvider(new Criteria(), true));
+
+                            LatLng myCurrLocation = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+                            mMap.addMarker(new MarkerOptions().position(myCurrLocation).title(m_Text));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(myCurrLocation));
+                        } // End of onClick(DialogInterface dialog, int whichButton)
+                    }); //End of alert.setPositiveButton
+                    alert.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            // Canceled.
+                            dialog.cancel();
+                        }
+                    }); //End of alert.setNegativeButton
+                    AlertDialog alertDialog = alert.create();
+                    alertDialog.show();
+            }
+        });
     }
 
 
@@ -34,6 +121,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -42,5 +130,60 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        LatLng seoul = new LatLng(37, 126);
+        mMap.addMarker(new MarkerOptions().position(seoul).title("In Seould Bitches"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(seoul));
+
+        LocationManager lManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+        Location myLocation = lManager.getLastKnownLocation(lManager.getBestProvider(new Criteria(), true));
+
+        LatLng myCurrLocation = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(16));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(myCurrLocation));
+        mMap.addCircle(new CircleOptions().center(myCurrLocation).radius(5));
+        mMap.addCircle(new CircleOptions().center(myCurrLocation).radius(300));
+
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
